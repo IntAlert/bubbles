1f1bbcbf2e7ad0afdf7d93c0b6ed1ce7d598078a
Alan Thomson <alan@projectmccoll.co.uk>
2016-10-19 12:13:24 +0200

    both graphs

--- web/public/javascripts/controllers/BubblesAdminController.js
+++ web/public/javascripts/controllers/BubblesAdminController.js
@@ -1,152 +1,78 @@
-app.controller('BubblesAdminController', function ($scope, $window, $location, $anchorScroll, TagService, FriendshipService, ScrapeService) {
+app.controller('BubblesAdminController', function ($scope, $window, $location, $anchorScroll, TagService, FriendshipService, ScrapeService, D3GraphOptionsService) {
 
+	$scope.ready = false;
 
 	// Load form options
 	$scope.tags = TagService;
 	$scope.scrapes = ScrapeService
 
-	// Query
-	$scope.query = {
-		scrape1: null,
-		scrape2: null
-	}
+	// Shared D3 Graph Options
+	$scope.d3options = D3GraphOptionsService
 
-	// Tags
-	$scope.scrape1Tags = []
-	$scope.scrape2Tags = []
+	// Two graphs
+	$scope.graphs = [
+		{
+			data: null,
+			api: null,
+			selectedScrape: null,
+			selectedTags: null
+		},
+		{
+			data: null,
+			api: null,
+			selectedScrape: null,
+			selectedTags: null
+		}
+	]
 
-	$scope.api = null;
+	// Wait for everything to be loaded
+	$scope.$watch('[scrapes.all,tags.all]', function(){
+		var ready = ($scope.scrapes.all.length > 0 && $scope.tags.all.length > 0)
+		if (ready) $scope.ready = true
+	}, true)
 
-
-
-
-	$scope.$watch('scrapes.all', function(){
-		$scope.query.scrape1 = $scope.scrapes.all[0]
-		$scope.query.scrape2 = $scope.scrapes.all[$scope.scrapes.all.length - 1]
+	// Once Everything is loaded
+	$scope.$watch('ready', function(){
+		if ($scope.ready) {
+			console.log('ready')
+			$scope.graphs[0].selectedScrape = $scope.scrapes.all[0] // first
+			$scope.graphs[1].selectedScrape = $scope.scrapes.all[$scope.scrapes.all.length - 1] // last	
+		}
 	})
+	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// D3 Graphs
-	$scope.graphs = {
-		d3options: null,
-		data: []
-	}
-	var color = d3.scale.category20()
-    $scope.graphs.d3options = {
-        chart: {
-            type: 'forceDirectedGraph',
-            height: 450,
-            width: 350, //(function(){ return nv.utils.windowSize().width - 850 })(),
-            margin:{top: 20, right: 20, bottom: 20, left: 20},
-            color: function(d){
-                return color(d.group)
-            },
-            charge: -1000,
-            linkDist: 120,
-            linkExtras: function(links) {
-            	
-            	links[0].forEach(function(link){
-
-            		if (link.__data__.source.filteredOutByTag || link.__data__.target.filteredOutByTag) {
-            			link.style.opacity = 0	
-            		} else {
-            			link.style.opacity = 1
-            		}
-            		
-            	})
-            },
-            nodeExtras: function(node) {
-            	
-            	node.style('opacity', function(d){
-            		if(d.filteredOutByTag) return '0.2';
-            		else return '1';
-            	})
-            	
-
-                node
-                  .append("text")
-	                  .attr("dx", 8)
-	                  .attr("dy", ".35em")
-	                  .text(function(d) { return d.name })
-                  .style('font-size', '17px');
-
-
-				// Append images
-				node.append("svg:g")
-			      .attr("class", "node")
-			      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-			 
-			  // Append a circle
-				node.append("svg:circle")
-			      .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
-			      .style("fill", "#eee");
-			 
-			   
-			  // Append profile image
-				node.append("svg:image")
-			        .attr("xlink:href",  function(d) { 
-			        	var profile_url = "http://graph.facebook.com/" + d.fb_id + "/picture?type=square"
-			        	return profile_url;
-			        })
-			        .attr("x", function(d) { return -25;})
-			        .attr("y", function(d) { return -25;})
-			        .attr("height",40)
-			        .attr("width", 40);
-            }
-        }
-    };
-
-
-    $scope.$watch('scrape1Tags', function(){
-    	if ($scope.query.scrape1)
-	    	$scope.graphs.data[0] = filterGraphByTag($scope.graphs.data[0], $scope.scrape1Tags)
+	// Update graph data on selectedTags change
+    $scope.$watch('graphs[0].selectedTags', function(){
+    	if ($scope.graphs[0].selectedScrape)
+	    	$scope.graphs[0].data = filterGraphByTag($scope.graphs[0].data, $scope.graphs[0].selectedTags)
     }, true)
 
-    
+    $scope.$watch('graphs[1].selectedTags', function(){
+    	if ($scope.graphs[1].selectedScrape)
+	    	$scope.graphs[1].data = filterGraphByTag($scope.graphs[1].data, $scope.graphs[1].selectedTags)
+    }, true)
 
-    // $scope.data = {
-    //     links: [],
-    //     nodes: []
-    // }
 
-	// Set graph data according to Scrape Date
-	$scope.$watch('query.scrape1', function(){
-		if ($scope.query.scrape1) {
-			FriendshipService.getGraphByScrapeId($scope.query.scrape1.id).then(function(graphData){
-		        // $scope.graphs.data[0] = graphData
-		        $scope.api.updateWithData(graphData)
+	// Update graph data on selectedScrape Change
+	$scope.$watch('graphs[0].selectedScrape', function(){
+		if ($scope.graphs[0].selectedScrape) {
+			FriendshipService.getGraphByScrapeId($scope.graphs[0].selectedScrape.id).then(function(graphData){
+		        $scope.graphs[0].api.updateWithData(filterGraphByTag(graphData, $scope.graphs[0].selectedTags))
 		    })	
 		}
-
-		console.log($scope.api)
 	})
-
-	$scope.$watch('query.scrape2', function(){
-		if ($scope.query.scrape2) {
-			FriendshipService.getGraphByScrapeId($scope.query.scrape2.id).then(function(graphData){
-		        $scope.graphs.data[1] = graphData
+	$scope.$watch('graphs[1].selectedScrape', function(){
+		if ($scope.graphs[1].selectedScrape) {
+			FriendshipService.getGraphByScrapeId($scope.graphs[1].selectedScrape.id).then(function(graphData){
+		        $scope.graphs[1].api.updateWithData(filterGraphByTag(graphData, $scope.graphs[1].selectedTags))
 		    })	
 		}
 	})
 
 
 	var filterGraphByTag = function(graphData, selectedTags) {
+
+		console.log(selectedTags)
 
 		angular.forEach(graphData.nodes, function(node) {
 
